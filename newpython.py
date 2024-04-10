5c464e98bcfd43d51b2118ab54525b8187945647
Swa22ti <104907735+Swa22ti@users.noreply.github.com>
2024-04-10 17:08:37 +0530

    Add files via upload

---
+++ newpython.py
@@ -0,0 +1,15 @@
+a=1
+b=2
+c=3
+a_str= str(a)
+b_str= str(b)
+c_str= str(c)
+final_str= a_str+b_str+c_str
+print (final_str)
+print(type(final_str))
+final_int= int(final_str)
+print(final_int)
+print(type(final_int))
+celcius= int(input())
+fahrenhite = (celcius * 9/5) + 32
+print (fahrenhite)
